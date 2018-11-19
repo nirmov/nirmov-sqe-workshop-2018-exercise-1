619912187d6383459a67e4ac099e6c0d6855ddb8
nir <movsho3@gmail.com>
2018-11-19 18:42:02 +0200

    commit- exept for

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,38 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-
+import {parseNewCode} from './Parser';
+var table = document.getElementById('Result');
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        addRow();
+        var Ans=parseNewCode(parsedCode,1);
+        var i=0;
+        for (i=0;i<Ans[0].length;i++) {
+            addWholeLine(Ans[0][i].line, Ans[0][i].type, Ans[0][i].name, Ans[0][i].condition, Ans[0][i].value);
+            addRow();
+        }
     });
 });
+var newRow;
+function addRow()
+{
+    newRow=table.insertRow(table.rows.length);
+}
+function addColumn(index,text)
+{
+    var newCell  = newRow.insertCell(index);
+    var newText  = document.createTextNode(text);
+    newCell.appendChild(newText);
+}
+function addWholeLine(line,type,name,condition,value)
+{
+    addColumn(0,line);
+    addColumn(1,type);
+    addColumn(2,name);
+    addColumn(3,condition);
+    addColumn(4,value);
+
+}
